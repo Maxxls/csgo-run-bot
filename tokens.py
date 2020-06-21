3cc8d28e1c665118b58ee777367f70490f30a7ab
Maximxls <maximxlssoff@gmail.com>
2020-06-21 23:55:09 +0300

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,5 +1,4 @@
 telegram_api_id = 123456 # obtain this from https://my.telegram.org/apps. For more info, visit https://github.com/Maxxls/csgo-run-bot/wiki/obtaining-telegram-api-id-and-hash
 telegram_api_hash = "api_hash" # obtain this from https://my.telegram.org/apps. For more info, visit https://github.com/Maxxls/csgo-run-bot/wiki/obtaining-telegram-api-id-and-hash
-telegram_phone_number = +12345678901 # your phone number
 
 csgorun_token = "put your csgorun authorization token here" # obtain this as written at For more info, visit https://github.com/Maxxls/csgo-run-bot/wiki/obtaining-csgorun-auth-key
